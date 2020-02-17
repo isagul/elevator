29fc4d5223f209c9dd7642692f79721936defad2
Isa Gul <isa.gul@beyn.com.tr>
2020-02-17 18:44:55 +0300

    html and css of building and elevator buttons added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,121 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="elevator-buttons-area">
+        <div className="floor-info-area">
+          <p class="floor-info">Kat Bilgisi</p>
+        </div>
+        <div className="elevator-buttons">
+          <div className="outer">
+            <button className="button">6</button>
+          </div>
+          <div className="outer">
+            <button className="button">5</button>
+          </div>
+          <div className="outer">
+            <button className="button">4</button>
+          </div>
+          <div className="outer">
+            <button className="button">3</button>
+          </div>
+          <div className="outer">
+            <button className="button">2</button>
+          </div>
+          <div className="outer">
+            <button className="button">1</button>
+          </div>
+          <div className="outer">
+            <button className="button">Z</button>
+          </div>
+        </div>
+      </div>
+      <div className="building">
+        <div className="floor">
+          <div className="home">
+            <div className="room">
+
+            </div>
+          </div>
+          <div className="home">
+            <div className="room">
+
+            </div>
+          </div>
+        </div>
+        <div className="floor">
+          <div className="home">
+            <div className="room">
+
+            </div>
+          </div>
+          <div className="home">
+            <div className="room">
+
+            </div>
+          </div>
+        </div>
+        <div className="floor">
+          <div className="home">
+            <div className="room">
+
+            </div>
+          </div>
+          <div className="home">
+            <div className="room">
+
+            </div>
+          </div>
+        </div>
+        <div className="floor">
+          <div className="home">
+            <div className="room">
+
+            </div>
+          </div>
+          <div className="home">
+            <div className="room">
+
+            </div>
+          </div>
+        </div>
+        <div className="floor">
+          <div className="home">
+            <div className="room">
+
+            </div>
+          </div>
+          <div className="home">
+            <div className="room">
+
+            </div>
+          </div>
+        </div>
+        <div className="floor">
+          <div className="home">
+            <div className="room">
+
+            </div>
+          </div>
+          <div className="home">
+            <div className="room">
+
+            </div>
+          </div>
+        </div>
+        <div className="floor">
+          <div className="home">
+            <div className="door"></div>
+            <div className="door"></div>
+          </div>
+          <div className="home"></div>
+        </div>
+        <div className="elevator">
+
+        </div>
+      </div>
     </div>
   );
 }
